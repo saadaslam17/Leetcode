fd0efbd3709395cbf8478f80a7cfb00416516048
saadaslam17 <141723915+saadaslam17@users.noreply.github.com>
2024-11-17 22:18:00 +0530

    [26] [Time Beats: 67.16%] [Memory Beats: 89.46%] - LeetPush

---
+++ Array/26-Remove-Duplicates-from-Sorted-Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int i = 1;
+
+        for (int j = 1; j < n; j++) {
+
+            if (nums[j] != nums[i - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+
+        }
+        return i;
+    }
+}
